b352ac327d5c837ac6f6acb13bbc7e7799537734
Keethesh <keethesh15@Gmail.com>
2019-11-10 11:21:04 +0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pronote Bruteforcer',
+    version='0.1',
+    packages=['selenium, proxyscrape, multiprocessing'],
+    url='https://github.com/keethesh/Pronote-Bruteforcer',
+    license='',
+    author='keethesh',
+    author_email='',
+    description='Bruteforces Pronote using proxies'
+)
